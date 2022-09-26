e4feb306741a5b975d9be520a035c1c56743650e
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2022-09-25 21:20:46 -0300

    Adicionando versão final agenda

--- Agenda/src/main.c
+++ Agenda/src/main.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 /*
-	NOME: CAROLINE SOUZA CAMARGO
+    NOME: CAROLINE SOUZA CAMARGO
 
-	1. O programa deve armazenar no máximo 10 pessoas
-	2. Todas as variáveis que forem usadas no programa deve ser ponteiros. A única exceção é o item 1 que deve ser uma variável global Pessoa pessoas[10].
-		- Os ponteiros devem apontar para um buffer chamado pBuffer onde os dados devem ser armazenados. Por exemplo, um contador para um for deve ser um ponteiro para int e o int vai ficar armazenado dentro de pBuffer. 
-		- Não pode usar struct dentro do pBuffer 3. 
+    PARTE 1:
+    Faça uma agenda capaz de incluir, apagar, buscar e listar quantas pessoas o usuário desejar, porém, toda a informação incluída na agenda deve ficar num único lugar chamado: “void *pBuffer”.
+    Não pergunte para o usuário quantas pessoas ele vai incluir. Não pode alocar espaço para mais pessoas do que o necessário.
+
+    PARTE 2:
+    Nenhuma variável pode ser declarada em todo o programa, somente ponteiros. Todos os dados do programa devem ser guardados dentro do pBuffer. Nem mesmo como parâmetro de função.  Só ponteiros que apontam para dentro do pBuffer. 
+    Exemplo do que não pode: int c; char a; int v[10]; void Funcao(int parametro)
+    Não pode usar struct em todo o programa. 
+    Usar fila ordenada (heap) para armazenar as pessoas em ordem alfabética sempre que o usuário incluir uma nova pessoa.  Implementar a base de dados da agenda usando lista duplamente ligada
+    Somente essa base de dados pode ficar fora do buffer principal, ou seja, pode usar um malloc para cada nodo.
 */
 
+//Funções
 int Menu(int *opMenu);
 char* NewElement(char *nome, int *idade, char *telefone);
-void Insert(char **tracer, char *last, char *newp);
-void UpdatePrevious (char **head, char *last);
+void Insert(char **tracer, char *newp);
+void UpdatePrevious (char **head);
 void Print(char **tracer);
 void Search(char **tracer, char *text);
-void Reset(char **head);
+void Remove(char **head);
+void Clear(char *tracer);
 
-/*
-void Inserir(int *quantPessoas);
-void Listar(int *quantPessoas, int *i);
-void Buscar(int *quantPessoas, int *i, char *nomeBusca);
-void Excluir(int *quantPessoas, int *i, int *teste, char *nomeBusca);
-*/
-
-void *pBuffer = NULL;
+//Ponteiros globais referentes ao início e fim da fila  
 char *start = NULL;
 char *last = NULL;
-const int TAMANHONOME = sizeof(char) * 15 + 1; 
-const int TAMANHOIDADE = sizeof(int);
-const int TAMANHOTELEFONE = sizeof(char) * 20 + 1; // (55) 51 9 9999-9999
-const int TAMANHOBASE = TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **) + sizeof(char **);
 
-int main()
-{
-	//ORDEM DOS DADOS NO PBUFFER: opMenu quantPessoas i teste tempNome tempIdade tempTelefone
-	int *opMenu = NULL, *quantPessoas = NULL, *i = NULL, *teste = NULL, *tempIdade = NULL;
-	char *tempNome  = NULL, *tempTelefone = NULL;
+//Dados do programa vão estar nesse ponteiro
+void *pBuffer = NULL;  
+
+//Constantes auxiliares 
+#define TAMANHONOME sizeof(char) * 15 + 1
+#define TAMANHOIDADE sizeof(int)
+#define TAMANHOTELEFONE sizeof(char) * 20 + 1  //(55) 51 9 9999-9999
+#define TAMANHOBASE TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **) + sizeof(char **)
+
+
+int main() {
+    //ORDEM DOS DADOS NO PBUFFER: opMenu quantPessoas i teste tempNome tempIdade tempTelefone
+    int *opMenu = NULL, *quantPessoas = NULL, *i = NULL, *teste = NULL, *tempIdade = NULL;
+    char *tempNome  = NULL, *tempTelefone = NULL;
 
     //Alocando espaço para o ponteiro void com as váriaveis gerais
-    pBuffer = (void*)(malloc((sizeof(int) * 5) + (sizeof(char) * 15 + 1) + (sizeof(char) * 20 + 1)));
+    pBuffer = (void *)(malloc((sizeof(int) * 5) + (sizeof(char) * 15 + 1) + (sizeof(char) * 20 + 1)));
     if (pBuffer == NULL){
         printf("Erro na alocacao de memoria");
         exit (1);
@@ -50,56 +55,60 @@ int main()
 
     //Definindo endereço incial para os ponteiros do pbuffer
     opMenu = (int *)(pBuffer);
-	quantPessoas = (int *)(pBuffer + sizeof(int));
-	i = (int *)(pBuffer + (sizeof(int) * 2));
-	teste = (int *)(pBuffer + (sizeof(int) * 3));
-	tempNome = (char *)(pBuffer + (sizeof(int) * 4));
-	tempIdade = (int *)(pBuffer + (sizeof(int) * 4) + ((sizeof(char) * 15) + 1));
-	tempTelefone = (char *)(pBuffer + (sizeof(int) * 5) + (sizeof(char) * 15 + 1));
-	
-	//Definindo conteúdo inicial
-	*opMenu = 0;
-	*quantPessoas = 0;
+    quantPessoas = (int *)(pBuffer + sizeof(int));
+    i = (int *)(pBuffer + (sizeof(int) * 2));
+    teste = (int *)(pBuffer + (sizeof(int) * 3));
+    tempNome = (char *)(pBuffer + (sizeof(int) * 4));
+    tempIdade = (int *)(pBuffer + (sizeof(int) * 4) + ((sizeof(char) * 15) + 1));
+    tempTelefone = (char *)(pBuffer + (sizeof(int) * 5) + (sizeof(char) * 15 + 1));
+    
+    //Definindo conteúdo inicial
+    *opMenu = 0;
+    *quantPessoas = 0;
 
     while (*opMenu != 5) {
         *opMenu = Menu(opMenu);
         switch (*opMenu) {
         case 1:
-			*quantPessoas = *quantPessoas + 1;
-			
-			printf("\n\tDigite o nome: \n");
-			scanf("%s", tempNome);
-			fflush(stdin);
-			printf("\n\tDigite a idade: \n");
-			scanf("%d", tempIdade);
-			fflush(stdin);
-			printf("\n\tDigite o telefone: \n");
-			scanf("%s", tempTelefone);
-			
-			Insert(&start, last, NewElement(tempNome, tempIdade, tempTelefone));
-			Print(&start);
+            //INCLUIR
+            printf("\n\tDigite o nome: \n");
+            scanf("%s", tempNome);
+            //fflush(stdin);
+            printf("\n\tDigite a idade: \n");
+            scanf("%d", tempIdade);
+           // fflush(stdin);
+            printf("\n\tDigite o telefone: \n");
+            scanf("%s", tempTelefone);
+            
+            *quantPessoas = *quantPessoas + 1;
+            Insert(&start, NewElement(tempNome, tempIdade, tempTelefone));
+            Print(&start);
             break;
 
         case 2:
-			//Excluir(quantPessoas, i, teste, nomeBusca);
-			//Listar(quantPessoas, i);
+            //APAGAR
+            Remove(&start);
+            Print(&start);
             break;
 
         case 3:
-			printf("\n\tDigite o nome que deseja buscar: \n");
-			scanf("%s", tempNome);
-			Search(&start, tempNome);
+            //BUSCAR
+            printf("\n\tDigite o nome que deseja buscar: \n");
+            scanf("%s", tempNome);
+            Search(&start, tempNome);
             break;
 
         case 4:
-			Print(&start);
+            //LISTAR
+            Print(&start);
             break;
 
-        case 5:
-			Reset(&start);
+        case 5: 
+            //SAIR
+            Clear(start);
             free(pBuffer);
-            printf("\nOpcao sair foi escolhida...\n");
-			return (0);
+            printf("\n\nOpcao sair foi escolhida...\n");
+            return (0);
             break;
         
         default:
@@ -120,165 +129,137 @@ int Menu(int *opMenu) {
     return *opMenu;
 }
 
-char* NewElement(char *nome, int *idade, char *telefone){
-	void *pBufferPessoas = NULL;
+//Cria o buffer, mas não faz as ligações ainda
+char* NewElement(char *nome, int *idade, char *telefone){ 
+    void *pBufferPessoas = NULL;
 
-	pBufferPessoas = (void*)(malloc(TAMANHOBASE));
-	if (pBufferPessoas == NULL){
+    pBufferPessoas = (void*)(malloc(TAMANHOBASE));
+    if (pBufferPessoas == NULL){
         printf("Erro na alocacao de memoria");
         exit (1);
     }
 
-	strcpy((char*)(pBufferPessoas), nome); //nome
-	*(int*)(pBufferPessoas + TAMANHONOME) = *idade; //idade
-	strcpy((char*)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE), telefone); //telefone
-	*(char **)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE) = NULL; //next
-	*(char **)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)) = NULL; //previous
+    strcpy((char *)(pBufferPessoas), nome); //nome
+    *(int *)(pBufferPessoas + TAMANHONOME) = *idade; //idade
+    strcpy((char*)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE), telefone); //telefone
+    *(char **)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE) = NULL; //next
+    *(char **)(pBufferPessoas + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)) = NULL; //previous
 
-	return pBufferPessoas;
+    return pBufferPessoas;
 }
 
-void Insert(char **head, char *last, char *newp){
-	char **tracer = head;
+//Coloca o buffer criado na fila encadeada, fazendo as Ligações dos ponteiros  
+void Insert(char **head, char *newp){
+    char **tracer = head;
 
-	while (*tracer != NULL && strcmp(*tracer, (char *)(newp)) < 1) {
-		tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);
-	}
+    //*tracer != NULL (Existe mais elementos na fila, next != Null)
+    //strcmp < 1 Enquanto não achar um elemento maior que o newp não sai do while
+    while (*tracer != NULL && strcmp(*tracer, (char *)(newp)) < 1) {     
+        tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE); //Vai para o próximo da fila
+    }
 
-	*(char **)(newp + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE) = *tracer;
-	*tracer = newp;
+    *(char **)(newp + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE) = *tracer;  //Atualiza o next do newp
+    *tracer = newp; //Atualiza o next do que vem antes do newp 
 
-	UpdatePrevious(head, last);
+    UpdatePrevious(head);
 }
 
-void UpdatePrevious (char **head, char *last){
-	char **tracer = head;
-	char *tracerPrevious = *head;
+//Atualiza o previous de todos elementos da fila encadeada
+void UpdatePrevious(char **tracer){ 
+    char *tracerPrevious = *tracer;
 
-	while (*tracer != NULL) {
-		tracerPrevious = *tracer; //Memoriza o endereço anterior 
-		tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);  //Vai para o próximo endereço da lista
-		if (*tracer != NULL) { 
-			*(char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)) = (char *) (tracerPrevious);
-		}
-	}
+    while (*tracer != NULL) {
+        tracerPrevious = *tracer; //Memoriza o endereço anterior 
+        tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);  //Vai para o próximo endereço da lista
+        if (*tracer != NULL) { //Certifica-se que existe um endereço válido
+            *(char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)) = (char *)(tracerPrevious);
+        }
+    }
 
-	//Atualiza o ponteiro last como o último elemento da lista
-	last = (char *)(tracerPrevious);
-	printf("\n\tNome: %s", (char *)(last));
+    //Atualiza o ponteiro last como o último elemento da lista
+    last = (char *)(tracerPrevious);
 }
 
+//Mostra todos elementos
 void Print(char **tracer){
-	int *quantPessoas =  (int *)(pBuffer + sizeof(int));
-	int *i =  (int *)(pBuffer + (sizeof(int) * 2));
+    //Ponteiros auxiliares que estão no pBuffer
+    int *quantPessoas =  (int *)(pBuffer + sizeof(int));
+    int *i =  (int *)(pBuffer + (sizeof(int) * 2));
 
-	printf("\n\n--- LISTA DAS PESSOAS ADICIONADOS ---");
-	printf("\n\tQuantidade total de Pessoas %d", *(int *)(quantPessoas));
-	printf("\n\tPrimeiro Nome da Lista: %s", (char *)(start));
-	printf("\n\tUltimo Nome da Lista: %s", (char *)(last));
+    printf("\n\n\n\t--- LISTA DAS PESSOAS ADICIONADOS NA FILA---\n");
+    printf("\n\tQuantidade total de Pessoas %d", *(int *)(quantPessoas));
+    printf("\n\tPrimeiro Nome da Lista: %s", (char *)(start));
+    printf("\n\tUltimo Nome da Lista: %s", (char *)(last));
 
-	for (*i = 0; *i < *quantPessoas; *i = *i + 1) {
-		printf("\n\n\tPESSOA %d", *(int *)(i));
-		printf("\n\tNome: %s", (char **)(*tracer));
-		printf("\n\tIdade: %d", *(int **)(*tracer + TAMANHONOME));
-		printf("\n\tTelefone: %s", (char **)(*tracer + TAMANHONOME + TAMANHOIDADE));
-		tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);
-	}	
+    //Faz a leitura pelo inicío da fila
+    for (*i = 0; *i < *quantPessoas; *i = *i + 1) {
+        printf("\n\n\tPESSOA %d", *(int *)(i));
+        printf("\n\tNome: %s", (char **)(*tracer));
+        printf("\n\tIdade: %d", *(int **)(*tracer + TAMANHONOME));
+        printf("\n\tTelefone: %s", (char **)(*tracer + TAMANHONOME + TAMANHOIDADE));
+        tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE); //Vai para o próximo elemento da fila
+    }   
 }
 
+//Pesquisa um nome na lista
 void Search(char **tracer, char *text){
-	char **temp;
-	int *teste = (int *)(pBuffer + (sizeof(int) * 3));
-	*teste = 0;
+    int *teste = (int *)(pBuffer + (sizeof(int) * 3));  //Ponteiro auxiliar que está no pBuffer
+    *teste = 0;
 
-	while (*tracer != NULL) {
-		if (strcmp(*tracer, text) == 0) {
-			*teste = 1;
-			break;
-		}
-		tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);
-	}
+    //Lê toda fila
+    while (*tracer != NULL) {
+        if (strcmp(*tracer, text) == 0) {  //Compara se o campo nome é igual
+            *teste = 1;
+            break;
+        }
+        tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE); //Vai para o próximo elemento da fila
+    }
 
-	if (*teste == 1){
-		printf("\n\tPALAVRA ENCONTRADA");
-		printf("\n\n\tNome: %s", (char **)(*tracer));
-		printf("\n\tIdade: %d", *(int **)(*tracer + TAMANHONOME));
-		printf("\n\tTelefone: %s", (char **)(*tracer + TAMANHONOME + TAMANHOIDADE));
+    if (*teste == 1){  
+        printf("\n\tPALAVRA ENCONTRADA");  //Mostra dados do elemento encontrado
+        printf("\n\n\tNome: %s", (char **)(*tracer));
+        printf("\n\tIdade: %d", *(int **)(*tracer + TAMANHONOME));
+        printf("\n\tTelefone: %s", (char **)(*tracer + TAMANHONOME + TAMANHOIDADE));
 
-		temp = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **));
-		printf("\n\n\tNome anterior na lista: %s", (char **)(*temp));
-		temp = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);
-		printf("\n\tProximo nome da lista: %s", (char **)(*temp));
-	} else {
-		printf("\nPalavra nao encontrada");
-	}
+        char **temp = NULL;   //Mostra quem vem antes e depois na fila
+        temp = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)); //Endereço do Previous
+        printf("\n\n\tNome anterior na fila: %s", (char **)(*temp));
+        temp = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);  //Endereço do Next
+        printf("\n\tProximo nome da fila: %s", (char **)(*temp));
+    } else {
+        printf("\nPalavra nao encontrada");
+    }
 }
 
-void Reset(char **head){
-	char **tracer = head;
-	void **old;
-	while (*tracer != NULL) {
-		old = *tracer;
-		*tracer = (char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);
-		free(*old);
-	} 
+void Remove(char **head){
+    //Ponteiros auxiliares que estão no pBuffer
+    int *quantPessoas =  (int *)(pBuffer + sizeof(int));
+    char *remove = NULL, **tracer = head;
+    
+    //Sempre remove a primeira pessoa da fila
+    if ((*tracer) != NULL){
+        remove = *tracer; //Memoriza o endereço de quem vai ser removido
+        *tracer =  *(char **)(remove + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE);  //Atualiza tracer para endereço do próximo elemento
+        *quantPessoas = *quantPessoas - 1; 
+        free(remove);
+        printf ("\n\nPESSOA RETIRADA DA FILA COM SUCESSO");
+    } else {
+        printf ("\n\nA FILA ESTA VAZIA");
+    }
+    
+    if ((*tracer) == NULL){  //Removeu o último elemento da lista
+        last = NULL;
+    } else { //Caso tenha mais de um pessoa apenas na fila, atualiza o previous do elemento que ficou
+        *(char **)(*tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE + sizeof(char **)) = NULL; //Endereço do previous fica NULL
+    }
 }
 
-/*
-void Inserir(int *quantPessoas){
-	fflush(stdin);
-	
-	printf("\n\tDigite o nome: \n");
-	scanf("%s", pessoas[*quantPessoas-1].nome);
-	fflush(stdin);
-	printf("\n\tDigite a idade: \n");
-	scanf("%d", &pessoas[*quantPessoas-1].idade);
-	fflush(stdin);
-	printf("\n\tDigite o telefone: \n");
-	scanf("%s", pessoas[*quantPessoas-1].telefone);
+//Exclui todos elementos da lista
+void Clear(char *tracer){
+    char *temp = NULL;
+    while (tracer != NULL) {
+        temp = *(char **)(tracer + TAMANHONOME + TAMANHOIDADE + TAMANHOTELEFONE); //Recebe o endereço do campo next para não perder
+        Remove(&tracer); //Remove o endereço atual
+        tracer = temp; //Atualiza o tracer para o próximo elemento
+    }
 }
-
-void Listar(int *quantPessoas, int *i){
-	for (*i = 0; *i < *quantPessoas; *i = *i + 1)	{
-		printf("\n\n\tPessoa %d", *i + 1);
-		printf("\n\tNome %s", pessoas[*i].nome);
-		printf("\n\tIdade %d", pessoas[*i].idade);
-		printf("\n\tTelefone %s", pessoas[*i].telefone);
-	}
-}
-
-void Buscar(int *quantPessoas, int *i, char *nomeBusca){
-    scanf ("%s" , nomeBusca);
-	printf("%s", nomeBusca);
-	for (*i = 0; *i < *quantPessoas; *i = *i + 1)	{
-		if (strcmp(nomeBusca, pessoas[*i].nome) == 0) {
-			printf("\n\tPessoa encontrada");
-			printf("\n\n\tPessoa %d", *i + 1);
-			printf("\n\tNome %s", pessoas[*i].nome);
-			printf("\n\tIdade %d", pessoas[*i].idade);
-			printf("\n\tTelefone %s", pessoas[*i].telefone);
-		}
-	}
-}
-
-void Excluir(int *quantPessoas, int *i, int *teste, char *nomeBusca){ 
-    printf("\nDigite a pessoa que deseja remover: \t");
-	scanf ("%s" , nomeBusca);
-	for (*i = 0; *i < *quantPessoas; *i = *i + 1)	{
-		if (strcmp(nomeBusca, pessoas[*i].nome) == 0) {
-			*teste = 1;
-		}
-		if (*teste == 1 && ((*i + 1) != *quantPessoas)) {
-			strcpy(pessoas[*i].nome, pessoas[*i + 1].nome);
-			pessoas[*i].idade = pessoas[*i + 1].idade;
-			strcpy(pessoas[*i].telefone, pessoas[*i + 1].telefone);
-		}
-	}
-	if (*teste == 1) {
-		*quantPessoas = *quantPessoas - 1;
-		printf("\nPessoa excluida com sucesso");
-	} else{
-		printf("\nPessoa nao encontrada");
-	}
-}
-*/
